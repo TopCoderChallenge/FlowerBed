21197cf4c7cbd04f5df79e10995bfa8567baa284
Core <hfreedomx@gmail.com>
2016-03-29 15:59:26 +0900

    Base code

---
+++ FlowerBed/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FlowerBed
+//
+//  Created by 侯 翔 on 2016/03/29.
+//  Copyright © 2016年 HX Studio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ FlowerBedTests/FlowerBedTests.swift
@@ -0,0 +1,36 @@
+//
+//  FlowerBedTests.swift
+//  FlowerBedTests
+//
+//  Created by 侯 翔 on 2016/03/29.
+//  Copyright © 2016年 HX Studio. All rights reserved.
+//
+
+import XCTest
+@testable import FlowerBed
+
+class FlowerBedTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
